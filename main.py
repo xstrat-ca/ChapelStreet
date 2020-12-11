42c20d8c90f0eb87e6828af9536a3715270cacf7
David Signoretti <david@signoretti.ca>
2020-12-11 16:57:34 -0500

    Init Commit Test 2020-12-10

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     try:
-        print_hi('Dave')
+        print_hi('Lisa')
     except:
         pass
 
